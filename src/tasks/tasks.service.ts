66351c5f33d04d8016061d3be26ac7396d306f9f
Kowal0 <krzysztof.kowalski@getresponse.com>
2020-01-07 22:53:00 +0100

    implement getTaskByID deleteTask and getAllTasksByFilter

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as uuid from 'uuid/v1';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,43 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if (search) {
+            tasks = tasks.filter(
+                task =>
+                    task.title.includes(search) ||
+                    task.description.includes(search),
+            );
+        }
+
+        return tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    deleteTask(id: string): Task[] {
+        return this.tasks.splice(
+            this.tasks.findIndex(task => task.id === id),
+            1,
+        );
+    }
+
+    updateTaskStatus(id: string, newStatus: TaskStatus) {
+        const task = this.getTaskById(id);
+        task.status = newStatus;
+
+        return task;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
